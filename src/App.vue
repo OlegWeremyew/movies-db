b1a83e409a3af86c28fcea9da710e07a41a6dc32
olegweremey <olegweremey1994@mail.ru>
2023-04-30 13:40:15 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <PosterBg :poster="posterBg" />
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import MoviesList from "@/components/MoviesList";
+import PosterBg from "@/components/PosterBg";
+
+export default {
+  name: "app",
+  components: {
+    MoviesList,
+    PosterBg
+  },
+  data: () => ({
+    posterBg: ""
+  }),
+  computed: {
+    ...mapGetters("movies", ["moviesList"])
+  },
+  methods: {
+    ...mapActions("movies", ["fetchMovies"]),
+    onChangePoster(poster) {
+      this.posterBg = poster;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  position: relative;
+}
+</style>
